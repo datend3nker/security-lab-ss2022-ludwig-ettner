5b8faf807537296ed3d75256d8e31dda3df2eef9
datend3nker <ludwig.ettner@hotmail.de>
2022-06-03 12:24:55 +0200

    fix

--- 1 - code breaking/decode.py
+++ 1 - code breaking/decode.py
@@ -9,7 +9,7 @@ for line in messages_raw:
 key = [222, 162, 247, 145, 216, 75, 68, 138, 120, 246, 168, 168, 54, 103, 200, 16, 248, 199, 39, 89, 119, 174, 95, 21, 94, 227, 202, 223]
 
 with open('decoded_text.txt', 'w') as message_file:
-        for line in message_serial:
-            for i, c in enumerate(line):
-                message_file.write(chr(key[i] ^ int(c, 16)))
-            message_file.write("\n")
+    for line in message_serial:
+        for i, c in enumerate(line):
+            message_file.write(chr(key[i] ^ int(c, 16)))
+        message_file.write("\n")
